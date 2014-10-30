2fac69e0b24a9556de36dd1fda0557a2c2a3af91
Andrew Fullford <afullford@netsocket.com>
2014-10-30 17:00:57 -0500

    dedicated maintainer email

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from distutils.core import setup
 
 setup(
 	name = "taskforce",
-	version = "0.0",
+	version = "0.1",
 	description = """Taskforce starts and restarts daemon processes.
 It will detect executable and/or module changes and automatically restart the affected processes.""",
 	author = "Andrew Fullford",
 	author_email = "git042013@fullford.com",
 	maintainer = "Andrew Fullford",
-	maintainer_email = "git042013@fullford.com",
+	maintainer_email = "pypi102014@fullford.com",
 	url = "https://github.com/akfullfo/taskforce",
-	download_url = "https://github.com/akfullfo/taskforce/archive/master.zip",
+	download_url = "git+git://github.com/akfullfo/taskforce.git"
 	license = "Apache License, Version 2.0",
 
 	packages = ['taskforce'],
