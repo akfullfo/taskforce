127f70f896326b6bc99155fdc3d0cdac53473323
Andrew Fullford <afullford@netsocket.com>
2014-10-31 15:16:00 -0500

    switch to setuptools.  added version file

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import re
+from setuptools import setup
+
+version_file = 'VERSION'
+with open(version_file, 'rt') as f:
+	version = f.readline().strip()
+
+if not re.match(r'^\d+(\.\d+)+$', version):
+	raise Exception("Invalid version '%s' found in version file '%s'" % (version, version_file))
 
 setup(
 	name = "taskforce",
-	version = "0.1.1",
+	version = version,
 	description = """Taskforce starts and restarts daemon processes.
 It will detect executable and/or module changes and automatically restart the affected processes.""",
 	author = "Andrew Fullford",
@@ -12,10 +20,10 @@ It will detect executable and/or module changes and automatically restart the af
 	maintainer = "Andrew Fullford",
 	maintainer_email = "pypi102014@fullford.com",
 	url = "https://github.com/akfullfo/taskforce",
-	download_url = "https://github.com/akfullfo/taskforce/tarball/0.1.1",
+	download_url = "https://github.com/akfullfo/taskforce/tarball/" + version,
 	license = "Apache License, Version 2.0",
 
 	packages = ['taskforce'],
 	scripts = ['bin/taskforce'],
-	requires = ['yaml'],
+	install_requires = ['yaml>=3.09'],
 )
