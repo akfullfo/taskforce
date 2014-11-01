7ab64b74645c72bdead7df3db45b0744f716e7a7
Andrew Fullford <git13@fullford.com>
2014-11-01 17:26:44 -0500

    setup changes

--- setup.py
+++ setup.py
@@ -3,18 +3,24 @@
 import re
 from setuptools import setup
 
-version_file = 'VERSION'
+version_file = 'taskforce/__init__.py'
+readme_file = 'README.md'
+version = ''
 with open(version_file, 'rt') as f:
-	version = f.readline().strip()
-
+	m = re.search(r'''__version__\s*=\s*["'](\d+(\.\d+)+)["']''', f.read())
+	if m:
+		version = m.group(1)
 if not re.match(r'^\d+(\.\d+)+$', version):
 	raise Exception("Invalid version '%s' found in version file '%s'" % (version, version_file))
+with open(readme_file, 'rt') as f:
+	long_description = f.read()
 
 setup(
 	name = "taskforce",
 	version = version,
 	description = """Taskforce starts and restarts daemon processes.
 It will detect executable and/or module changes and automatically restart the affected processes.""",
+	long_description = long_description,
 	author = "Andrew Fullford",
 	author_email = "git042013@fullford.com",
 	maintainer = "Andrew Fullford",
@@ -22,8 +28,10 @@ It will detect executable and/or module changes and automatically restart the af
 	url = "https://github.com/akfullfo/taskforce",
 	download_url = "https://github.com/akfullfo/taskforce/tarball/" + version,
 	license = "Apache License, Version 2.0",
+	include_package_data = True,
 
 	packages = ['taskforce'],
+	package_data = {'': ['*.txt']},
 	scripts = ['bin/taskforce'],
 	install_requires = ['yaml>=3.09'],
 )
