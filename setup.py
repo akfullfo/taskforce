d84ab0ad9bc3fedd4bdabb8ae552cc39ceba6273
Andrew Fullford <afullford@netsocket.com>
2014-11-01 19:31:32 -0500

    made the readme optional

--- setup.py
+++ setup.py
@@ -12,26 +12,29 @@ with open(version_file, 'rt') as f:
 		version = m.group(1)
 if not re.match(r'^\d+(\.\d+)+$', version):
 	raise Exception("Invalid version '%s' found in version file '%s'" % (version, version_file))
-with open(readme_file, 'rt') as f:
-	long_description = f.read()
 
-setup(
-	name = "taskforce",
-	version = version,
-	description = """Taskforce starts and restarts daemon processes.
+setup_parms = {
+	'name': "taskforce",
+	'version': version,
+	'description': """Taskforce starts and restarts daemon processes.
 It will detect executable and/or module changes and automatically restart the affected processes.""",
-	long_description = long_description,
-	author = "Andrew Fullford",
-	author_email = "git042013@fullford.com",
-	maintainer = "Andrew Fullford",
-	maintainer_email = "pypi102014@fullford.com",
-	url = "https://github.com/akfullfo/taskforce",
-	download_url = "https://github.com/akfullfo/taskforce/tarball/" + version,
-	license = "Apache License, Version 2.0",
-	include_package_data = True,
+	'author': "Andrew Fullford",
+	'author_email': "git042013@fullford.com",
+	'maintainer': "Andrew Fullford",
+	'maintainer_email': "pypi102014@fullford.com",
+	'url': "https://github.com/akfullfo/taskforce",
+	'download_url': "https://github.com/akfullfo/taskforce/tarball/" + version,
+	'license': "Apache License, Version 2.0",
+	'include_package_data': True,
 
-	packages = ['taskforce'],
-	package_data = {'': ['*.txt']},
-	scripts = ['bin/taskforce'],
-	install_requires = ['yaml>=3.09'],
-)
+	'packages': ['taskforce'],
+	'scripts': ['bin/taskforce'],
+	'install_requires': ['yaml>=3.09'],
+}
+try:
+	with open(readme_file, 'rt') as f:
+		setup_parms['long_description'] = f.read()
+except:
+	pass
+
+setup(**setup_parms)
