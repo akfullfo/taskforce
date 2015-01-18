f8cd1f3fac0ec105a726569b4ee3eceabf689302
Andrew Fullford <git13@fullford.com>
2015-01-18 14:40:54 -0600

    include a POST example

--- taskforce/httpd.py
+++ taskforce/httpd.py
@@ -19,12 +19,15 @@
 #
 
 import sys, errno, re, logging
+from cgi import parse_header, parse_multipart
 try:
 	import socketserver
 	import http.server as http_server
+	from urllib.parse import parse_qs, urlparse
 except:
 	import SocketServer as socketserver
 	import BaseHTTPServer as http_server
+	from urlparse import parse_qs, urlparse
 
 from __init__ import __version__ as taskforce_version
 
@@ -35,29 +38,62 @@ class HTTP_handler(http_server.BaseHTTPRequestHandler):
 	#sys_version = ''
 
 	def fault(self, code, message):
-		self.server.log.error("HTTP %d on '%s' -- %s", code, self.path, message)
 		self.send_response(code)
 		if code < 500:
+			self.server.log.warning("HTTP %d on '%s' -- %s", code, self.path, message)
 			message = message.encode('utf-8')
 			self.send_header("Content-Type", "text/plain")
 			self.send_header("Content-Length", len(message))
 			self.end_headers()
 			self.wfile.write(message)
 		else:
+			self.server.log.error("HTTP %d on '%s' -- %s", code, self.path, message, exc_info=True)
 			self.end_headers()
 
 	def do_GET(self):
 		try:
-			resp = self.server.serve(self.path)
+			resp = self.server.serve_get(self.path)
 			if not resp:
 				self.fault(404, self.path + ' not found')
 				return
 			if type(resp) != tuple or len(resp) != 2:
-				self.fault(404, 'Bad callback response for ' + self.path)
+				self.fault(500, 'Bad callback response for ' + self.path)
 			content, content_type = resp
 		except Exception as e:
 			self.fault(500, "Callback error -- " + str(e))
 			return
+		content = content.encode('utf-8')
+		self.send_response(200)
+		self.send_header("Content-Type", content_type)
+		self.send_header("Content-Length", len(content))
+		self.end_headers()
+		self.wfile.write(content)
+
+	def do_POST(self):
+		try:
+			ctype, pdict = parse_header(self.headers['content-type'])
+			if ctype == 'multipart/form-data':
+				postdict = parse_multipart(self.rfile, pdict)
+			elif ctype == 'application/x-www-form-urlencoded':
+				length = int(self.headers['content-length'])
+				postdict = parse_qs(self.rfile.read(length), keep_blank_values=1)
+			else:
+				postdict = {}
+		except Exception as e:
+			self.fault(400, "Parse error -- " + str(e))
+			return
+		try:
+			resp = self.server.serve_post(self.path, postdict)
+			if not resp:
+				self.fault(404, self.path + ' not found')
+				return
+			if type(resp) != tuple or len(resp) != 2:
+				self.fault(500, 'Bad callback response for ' + self.path)
+			content, content_type = resp
+		except Exception as e:
+			self.fault(500, "Callback error -- " + str(e))
+			return
+
 		content = content.encode('utf-8')
 		self.send_response(200)
 		self.send_header("Content-Type", content_type)
@@ -73,7 +109,7 @@ class Server(socketserver.ThreadingMixIn, socketserver.TCPServer, object):
 	via taskforce.poll(), select.select(), etc.  When activiity is detected,
 	the handle_request() method should be invoked.  This starts a thread to
 	handle the request.  URL paths are handled with callbacks which need
-	to be established before any activity might occur.  If not callback
+	to be established before any activity might occur.  If no callback
 	is registered for a given path, the embedded handler will report a
 	404 error.  Any exceptions raised by the callback result in a 500
 	error.
@@ -101,7 +137,8 @@ class Server(socketserver.ThreadingMixIn, socketserver.TCPServer, object):
 		else:
 			self.log = logging.getLogger(__name__)
 			self.log.addHandler(logging.NullHandler())
-		self.registrations = {}
+		self.get_registrations = {}
+		self.post_registrations = {}
 
 	def get_request(self):
 		info = super(Server, self).get_request()
@@ -109,21 +146,49 @@ class Server(socketserver.ThreadingMixIn, socketserver.TCPServer, object):
 			info[0].settimeout(self.timeout)
 		return info
 
-	def register(self, regex, callback):
+	def register_get(self, regex, callback):
 		"""
 		Register a regex for processing HTTP GET
 		requests.  If the callback is None, any
 		existing registration is removed.
 	"""
 		if callback is None:
-			if regex in self.registrations:
-				del self.registrations[regex]
-			return
-		self.registrations[regex] = (re.compile(regex), callback)
+			if regex in self.get_registrations:
+				del self.get_registrations[regex]
+		else:
+			self.get_registrations[regex] = (re.compile(regex), callback)
 
-	def serve(self, path):
+	def register_post(self, regex, callback):
 		"""
-		Find a callback for the given HTTP path, call it and
+		Register a regex for processing HTTP POST
+		requests.  If the callback is None, any
+		existing registration is removed.
+
+		The callback will be called as:
+	
+			callback(path, postdict)
+	"""
+		if callback is None:
+			if regex in self.post_registrations:
+				del self.post_registrations[regex]
+		else:
+			self.post_registrations[regex] = (re.compile(regex), callback)
+
+	def _match_path(self, path, registrations):
+		match_len = 0
+		matched = None
+		for ex, callback in registrations.values():
+			m = ex.match(path)
+			if m:
+				l = len(m.group(0))
+				if l > match_len:
+					match_len = l
+					matched = callback
+		return matched
+
+	def serve_get(self, path):
+		"""
+		Find a GET callback for the given HTTP path, call it and
 		return the results.  The callback is called with
 		one argument, the path used to match it.  The callback
 		must return a tuple:
@@ -139,19 +204,33 @@ class Server(socketserver.ThreadingMixIn, socketserver.TCPServer, object):
 	"""
 		if path is None: return None
 
-		matched = None
-		match_len = 0
-		for ex, callback in self.registrations.values():
-			m = ex.match(path)
-			if m:
-				l = len(m.group(0))
-				if l > match_len:
-					match_len = l
-					matched = callback
+		matched = self._match_path(path, self.get_registrations)
 		if matched is None:
 			return None
 		else:
 			return matched(path)
+
+	def serve_post(self, path, postdict):
+		"""
+		Find a POST callback for the given HTTP path, call it and
+		return the results.  The callback is called with the path
+		used to match it and a dict of vars from the POST body.
+		The callback must return a tuple:
+
+			(content, content_type)
+
+		If multiple registrations match the path, the one with
+		the longest matching text will be used.  Matches are
+		always anchored at the start of the path.
+
+		None is returned if no registered callback is willing
+		to handle a path.
+	"""
+		matched = self._match_path(path, self.post_registrations)
+		if matched is None:
+			return None
+		else:
+			return matched(path, postdict)
 
 if __name__ == "__main__":
 	import argparse, json
@@ -175,7 +254,7 @@ if __name__ == "__main__":
 			u'Zhōngguó': u'nǐ hǎo shìjiè',
 		}
 		if args.json:
-			return (json.dumps(ans, indent=4), 'application/json')
+			return (json.dumps(ans, indent=4)+'\n', 'application/json')
 		else:
 			text = """<html>
 <head>
@@ -195,6 +274,29 @@ if __name__ == "__main__":
 </html>
 """
 			return (text, 'text/html; charset=utf-8')
+
+	def poster(path, postdict):
+		global log
+		u = urlparse(path)
+		log.info("For path: %s", path)
+		p = postdict.copy()
+		if u.query:
+			q = parse_qs(u.query)
+			log.debug("Query held: %s", str(q))
+			p.update(q)
+		q = {}
+		for tag in p:
+			vals = []
+			for v in p[tag]:
+				if type(v) is not str:
+					v = v.decode('utf-8')
+				vals.append(v)
+			if type(tag) is not str:
+				tag = tag.decode('utf-8')
+			q[tag] = vals
+		for tag in sorted(q):
+			log.info("\t%s = '%s'", tag, ','.join(q[tag]))
+		return ('ok\n', 'text/plain')
 
 	p = argparse.ArgumentParser(description="Manage tasks and process pools")
 
@@ -232,7 +334,8 @@ if __name__ == "__main__":
 
 	httpd = Server(host=host, port=port, timeout = 5, log = log)
 
-	httpd.register(r'/.*', getter)
+	httpd.register_get(r'/.*', getter)
+	httpd.register_post(r'/.*', poster)
 
 	pset = poll.poll()
 	pset.register(httpd, poll.POLLIN)
