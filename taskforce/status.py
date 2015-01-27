022824d1de00029b954c195e194c0d3facda7850
Andrew Fullford <git13@fullford.com>
2015-01-27 11:02:46 -0600

    add config dump to status interface

--- taskforce/status.py
+++ taskforce/status.py
@@ -47,15 +47,97 @@ class http(object):
 		self._legion = legion
 		self._httpd = httpd
 
-		self._httpd.register_get(r'/status/tasks', self.status)
-		self._httpd.register_post(r'/status/tasks', self.status)
+		self._httpd.register_get(r'/status/tasks', self.tasks)
+		self._httpd.register_post(r'/status/tasks', self.tasks)
+		self._httpd.register_get(r'/status/config', self.config)
+		self._httpd.register_post(r'/status/config', self.config)
 
-	def status(self, path, postmap=None):
+	def config(self, path, postmap=None):
+		"""
+		Return the running configuration which almost always matches the
+		configuration in the config file.  During a reconfiguration, it may
+		be transitioning to the new state, in which case it will be different
+		to the pending config.	Neither the running or pending configurations
+		necessarily match the operational state, either because a task has
+		exited and not yet restarted, or because a task control has been
+		changed via the management interface.
+
+		Options:
+		  fmt		-  Placeholder for other content formatting (eg XML).
+		  		   Currently only "json" is supported.
+		  pending	-  If set to "1", return the pending config instead of
+		  		   the running config.
+	"""
+
 		q = httpd.merge_query(path, postmap)
 		if 'fmt' in q:
 			fmt = q['fmt']
 		else:
 			fmt = 'json'
+
+		params = {}
+		try: params['indent'] = int(q.get('indent')[0])
+		except: pass
+		print(params)
+
+		pending = httpd.truthy(q.get('pending'))
+		if pending:
+			ans = self._legion._config_pending
+		else:
+			ans = self._legion._config_running
+
+		if fmt == 'json':
+			return (200, json.dumps(ans, **params)+'\n', 'application/json')
+		else:
+			return (415, 'Invalid "fmt" request, supported formats are: json\n', 'text/plain')
+
+	def tasks(self, path, postmap=None):
+		"""
+		Return the task status.  This delves into the operating structures
+		and picks out information about tasks that is useful for status
+		monitoring.
+
+		For each task, the response includes:
+
+		  control	- The active task control value, whoich may have been
+		  		  changed via the management interface.
+		  count		- The number of processes configured to run for the
+		  		  task.  This does not necessarily correspond to the
+				  process list below if tasks are failing or the
+				  control is set to "off".
+		  processes	- A list of the running processes for the task.
+		  		  Each entry may contain:
+				    pid		- The process ID of the process currently
+				    		  running in this slot.  If "pid" is not
+						  present, no process is running in the
+						  slot.
+				    started	- The ISO8601 date stamp when the
+				    		  process started.
+				    started_t	- The Unix time_t of when the process
+				    		  started.
+				    status	- The exit code for the last time this
+				    		  process exited.
+				    exit	- The status translated for human
+				    		  consumption.
+
+		Not that the status and exit values are not cleared if the process
+		has successfully restarted.
+
+		Options:
+		  fmt		-  Placeholder for other content formatting (eg XML).
+		  		   Currently only "json" is supported.
+		  indent	-  Indent to make formatted output more human-readable.
+		  		   Default is no indent which removes unnecessary padding.
+	"""
+		q = httpd.merge_query(path, postmap)
+		if 'fmt' in q:
+			fmt = q['fmt']
+		else:
+			fmt = 'json'
+		params = {}
+		try: params['indent'] = int(q.get('indent')[0])
+		except: pass
+
 		ans = {}
 		for name, tinfo in self._legion._tasknames.items():
 			t = tinfo[0]
@@ -79,6 +161,6 @@ class http(object):
 			ans[name] = info
 
 		if fmt == 'json':
-			return (200, json.dumps(ans, indent=4)+'\n', 'application/json')
+			return (200, json.dumps(ans, **params)+'\n', 'application/json')
 		else:
 			return (415, 'Invalid "fmt" request, supported formats are: json\n', 'text/plain')
