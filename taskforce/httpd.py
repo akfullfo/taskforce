060e3712d9faffda244c6dc2bc3fe1a0e78cf033
Andrew Fullford <git13@fullford.com>
2015-09-05 14:56:25 -0500

    Add =data and =type fields to handle case where the post payload is not form-data or urlencoded

--- taskforce/httpd.py
+++ taskforce/httpd.py
@@ -146,6 +146,10 @@ class HTTP_handler(http_server.BaseHTTPRequestHandler):
 				elif ctype == 'application/x-www-form-urlencoded':
 					length = int(self.headers['content-length'])
 					postmap = parse_qs(self.rfile.read(length), keep_blank_values=1)
+				else:
+					length = int(self.headers['content-length'])
+					postmap['=type'] = ctype
+					postmap['=data'] = self.rfile.read(length)
 			except Exception as e:
 				self.fault(400, "Parse error -- " + str(e))
 				return
